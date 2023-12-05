ca6f4a9abe58a20d74205ae87b9524a78be0fac9
Lincy <lincgr@gmail.com>
2023-12-05 17:02:43 -0600

    02_01 Combinar dos dataframes en Scala

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,55 @@
+import org.apache.spark.sql.{SparkSession, Row}
+import org.apache.spark.sql.types.{
+  StructType,
+  StructField,
+  StringType,
+  IntegerType
+}
+
+@main def main() = {
+
+  val estructuraDatosPersonal = new StructType()
+    .add(StructField("id", IntegerType, true))
+    .add(StructField("nombre", StringType, true))
+    .add(StructField("apellido", StringType, true))
+    .add(StructField("email", StringType, true))
+
+  val estructuraDatosPuesto = new StructType()
+    .add(StructField("id", IntegerType, true))
+    .add(StructField("puesto", StringType, true))
+    .add(StructField("estado", StringType, true))
+
+  val spark = SparkSession.builder
+    .appName("Dataframes")
+    .master("local")
+    .getOrCreate()
+
+  val datosPersonal = Seq(
+    Row(15379, "Hernan", "Maldonado", "hmaldonado@kinetecoinc.com"),
+    Row(15419, "Carlos", "Zambrano", "czambrano@kinetecoinc.com"),
+    Row(15420, "Elena", "Domínguez", "edomínguez@kinetecoinc.com"),
+    Row(46556, "Teresa", "Benítez", "tbenítez@kinetecoinc.com")
+  )
+
+  val datosPuesto = Seq(
+    Row(15379, "Ingeniero I", "Texas"),
+    Row(15419, "Vicetresidente RRHH", "California"),
+    Row(15420, "Manufacturación y distribución", "California"),
+    Row(46556, "Geólogo IV", "Maryland")
+  )
+
+  val dfPersonal = spark.createDataFrame(
+    spark.sparkContext.parallelize(datosPersonal),
+    estructuraDatosPersonal
+  )
+  dfPersonal.show()
+
+  val dfPuesto = spark.createDataFrame(
+    spark.sparkContext.parallelize(datosPuesto),
+    estructuraDatosPuesto
+  )
+  dfPuesto.show()
+
+  val unionDataframe = dfPersonal.join(dfPuesto, Seq("id"), "inner")
+  unionDataframe.show()
+}
