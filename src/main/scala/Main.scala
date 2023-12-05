fae14fc4cbca23b34582b5c067dddd83f59e2a9e
Lincy <lincgr@gmail.com>
2023-12-05 10:58:00 -0600

    01_07 Crear un tupla a partir de dos listas en Scala

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+@main def main(): Unit = {
+    
+    val  listaNombre= List("Carlos", "Laura", "Luis", "Ana", "Javier", "Rosa")
+    val listaEdad  = List(30, 25, 35, 28, 40)
+           
+    println(s"Lista de Nombres: $listaNombre")
+    println(s"Lista de Edades: $listaEdad")
+  }
