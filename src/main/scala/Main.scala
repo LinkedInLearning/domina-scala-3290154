afd8348a7ce0605caa80f6b692f3ce8a42459d88
Lincy <lincgr@gmail.com>
2023-12-06 20:41:53 -0600

    02_08 Unir dos listas en Scala

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+@main def main() = {
+  val listaID2022 = List(15379, 15419, 15420, 46556)
+  val listaID2023 = List(32332, 46554, 46766, 35545, 53706, 34797)
+
+  println(s"Lista Original 1: $listaID2022")
+  println(s"Lista Original 2: $listaID2023")
+}
