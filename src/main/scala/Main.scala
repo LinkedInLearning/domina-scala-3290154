976da5922f551bb9d0cbfbd1b461090134d76b4a
Lincy <lincgr@gmail.com>
2023-12-07 19:01:47 -0600

    03_05 Funciones parciales en Scala

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,23 @@
+@main def main() = {
+  val datosPersonal = Seq(
+    (15379, "Hernan", "Maldonado", "hmaldonado@kinetecoinc.com"),
+    (15419, "Carlos", "Zambrano", "czambrano@kinetecoinc.com"),
+    (15420, "Elena", "Domínguez", "edomínguez@kinetecoinc.com"),
+    (46556, "Teresa", "Benítez", "tbenítez@kinetecoinc.com"),
+    (46555, "Sonia", "Blanco", "sblanco@kinetecoinc.com"),
+    (46559, "Ernesto", "García", "egarcía@kinetecoinc.com"),
+    (15428, "Rodrigo", "Torres", "rtorres@kinetecoinc.com"),
+    (15429, "Roberto", "Molina", "rmolina@kinetecoinc.com"),
+    (47130, "Nuria", "Flores", "nflores@kinetecoinc.com"),
+    (32117, "Patricia", "Bermúdez", "pbermúdez@kinetecoinc.com"),
+    (32313, "Clara", "Ortiz", "cortiz@kinetecoinc.com")
+  )
+
+  val aumentoSalaria: PartialFunction[(Int, String, String, String), String] = {
+    case (id, __, _, _) if id <= 32117 => s"$id tiene aumento"
+  }
+
+  val resultados = datosPersonal.collect(aumentoSalaria)
+
+  resultados.foreach(println)
+}
